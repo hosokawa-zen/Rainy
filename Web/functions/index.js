a2f967e26ea6bd131e25b7a90df6d52ec4a69609
Hosokawa Zen <56865797+hosokawa-zen@users.noreply.github.com>
2021-07-08 01:13:48 +0900

    cloud function cors issue

--- Web/functions/index.js
+++ Web/functions/index.js
@@ -2,6 +2,7 @@ const functions = require("firebase-functions");
 const admin = require("firebase-admin")
 admin.initializeApp();
 const database = admin.database()
+const cors = require('cors')({origin: true});
 
 const http = require('http');
 //const stripe = require('stripe')('sk_test_4eC39HqLyjWDarjtT1zdp7dc');
@@ -10,25 +11,27 @@ const stripe = require('stripe')('sk_test_51J9vm5Cj4zJmU9hW2oOlFr6O5fJgJdZVpN3bi
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 
-exports.pay = functions.https.onRequest(async (request, response) => {
-  const amount = request.query.amount
-  const fee = request.query.fee
-  const accountID = request.query.accountID
-  try {
-    const paymentIntent =
-      await stripe.paymentIntents.create({
-        payment_method_types: ['card'],
-        amount: amount * 100,
-        currency: 'usd',
-        application_fee_amount: fee * 100,
-        transfer_data: {
-          destination: accountID,
-        },
-      });
-    return response.send(JSON.stringify(paymentIntent));
-  } catch (error) {
-    return response.status(500).send(error.message);
-  }
+exports.pay = functions.https.onRequest((request, response) => {
+  cors(request, response, async () => {
+    const amount = request.query.amount
+    const fee = request.query.fee
+    const accountID = request.query.accountID
+    try {
+      const paymentIntent =
+        await stripe.paymentIntents.create({
+          payment_method_types: ['card'],
+          amount: amount * 100,
+          currency: 'usd',
+          application_fee_amount: fee * 100,
+          transfer_data: {
+            destination: accountID,
+          },
+        });
+      return response.send(JSON.stringify(paymentIntent));
+    } catch (error) {
+      return response.status(500).send(error.message);
+    }
+  })
 });
 
 function generateAccountLink(accountID, origin) {
@@ -41,46 +44,50 @@ function generateAccountLink(accountID, origin) {
 }
 
 exports.onboard = functions.https.onRequest(async (request, response) => {
-  try {
-    const account = await stripe.accounts.create({
-      type: 'express',
-      country: 'US',
-      capabilities: {
-        card_payments: {requested: true},
-        transfers: {requested: true},
-      },
-    });
-    const redirect = request.query.redirect
-    const businessID = request.query.businessID
-    var origin = redirect ? `${redirect}?businessParam=${businessID}` : `${request.headers.host}?businessParam=${businessID}`;
-    const accountLinkURL = await generateAccountLink(account.id, origin);
+  cors(request, response, async () => {
+    try {
+      const account = await stripe.accounts.create({
+        type: 'express',
+        country: 'US',
+        capabilities: {
+          card_payments: {requested: true},
+          transfers: {requested: true},
+        },
+      });
+      const redirect = request.query.redirect
+      const businessID = request.query.businessID
+      var origin = redirect ? `${redirect}?businessParam=${businessID}` : `${request.headers.host}?businessParam=${businessID}`;
+      const accountLinkURL = await generateAccountLink(account.id, origin);
 
-    var updates = {}
-    updates[`business/${businessID}/accountID`] = account.id;
-    updates[`business/${businessID}/confirmBusiness`] = false;
-    await database.ref().update(updates);
+      var updates = {}
+      updates[`business/${businessID}/accountID`] = account.id;
+      updates[`business/${businessID}/confirmBusiness`] = false;
+      await database.ref().update(updates);
 
-    return response.send({url: accountLinkURL, accountID: account.id, account, businessID});
-  } catch (error) {
-    return response.status(500).send(error.message);
-  }
+      return response.send({url: accountLinkURL, accountID: account.id, account, businessID});
+    } catch (error) {
+      return response.status(500).send(error.message);
+    }
+  })
 })
 
 exports.onboardRefresh = functions.https.onRequest(async (request, response) => {
-  try {
-    const accountID = request.query.accountID
-    const redirect = request.query.redirect
-    const businessID = request.query.businessID
-    var origin = redirect ? `${redirect}?businessParam=${businessID}` : `${request.headers.host}?businessParam=${businessID}`;
-    const accountLinkURL = await generateAccountLink(accountID, origin);
+  cors(request, response, async () => {
+    try {
+      const accountID = request.query.accountID
+      const redirect = request.query.redirect
+      const businessID = request.query.businessID
+      var origin = redirect ? `${redirect}?businessParam=${businessID}` : `${request.headers.host}?businessParam=${businessID}`;
+      const accountLinkURL = await generateAccountLink(accountID, origin);
 
-    var updates = {}
-    updates[`business/${businessID}/accountID`] = account.id;
-    updates[`business/${businessID}/confirmBusiness`] = false;
-    await database.ref().update(updates);
+      var updates = {}
+      updates[`business/${businessID}/accountID`] = account.id;
+      updates[`business/${businessID}/confirmBusiness`] = false;
+      await database.ref().update(updates);
 
-    return response.send({url: accountLinkURL});
-  } catch (error) {
-    return response.status(500).send(error.message);
-  }
+      return response.send({url: accountLinkURL});
+    } catch (error) {
+      return response.status(500).send(error.message);
+    }
+  });
 })
